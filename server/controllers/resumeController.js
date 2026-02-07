26958f5049ddb63f6531fd60a801e1bd9caf8af4
gayathribhuv <gb25abh@herts.ac.uk>
2026-02-08 00:49:31 +0300

    Fix Gemini import and prompt template string

--- server/controllers/resumeController.js
+++ server/controllers/resumeController.js
@@ -1,9 +1,10 @@
 // server/controllers/resumeController.js
 
+const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 exports.generateResume = async (req, res) => {
   try {
-    const { GoogleGenerativeAI } = require("@google/generative-ai");
+    
     const generateResumeDocxFromTemplate = require("../utils/generateResumeDocxFromTemplate");
 
     const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
